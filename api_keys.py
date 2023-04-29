0b49300ec9a7bd67451dcbefc9dc32a0643f4fc7
laurahp00 <32912181+laurahp00@users.noreply.github.com>
2023-04-29 15:33:07 -0400

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,7 @@
 # OpenWeatherMap API Key
-weather_api_key = "afc512a3a148aec6fa8e6795b1413172"
+weather_api_key = ""
 
 # Geoapify API Key
-geoapify_key = "0a812b78313647728afe94c2de2d6e43"
+geoapify_key = ""
+
+# i removed them for this project for my financial security
